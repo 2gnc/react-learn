cbfbecf49cf29d93c5ee7b5bb888eba301b21c24
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2018-02-07 10:09:27 +0300

    лекция 27

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,51 +1,68 @@
+class IndecisionApp extends React.Component {
+	render() {
+		return (
+			<div>
+				<Header />
+				<Action />
+				<Options />
+				<AddOption />
+			</div>
+		);
+	}
+}
+
 class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Indecision</h1>
-        <h2>Put your life in the hands of a computer</h2>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<h1>Indecision</h1>
+				<h2>Put your life in the hands of a computer</h2>
+			</div>
+		);
+	}
 }
 
 class Action extends React.Component {
-  render() {
-    return (
-      <div>
-        <button>What should I do?</button>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<button>What should I do?</button>
+			</div>
+		);
+	}
+}
+
+class Option extends React.Component {
+	render() {
+		return (
+			<div>
+				Option component here
+			</div>
+			)
+	}
 }
 
 class Options extends React.Component {
-  render() {
-    return (
-      <div>
-        Options component here
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				Options component here
+				<Option />
+				<Option />
+				<Option />
+		</div>
+		);
+	}
 }
 
 class AddOption extends React.Component {
-  render() {
-    return (
-      <div>
-        AddOption component here
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				AddOption component here
+			</div>
+		);
+	}
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
