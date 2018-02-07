47a2fbcbcf767a8a54ab002cc32a36c549e93c89
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2018-02-07 09:20:01 +0300

    лекция 26

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,16 +1,51 @@
-class Header extends React.Componet {
-	render() {
-		return <p>This is from Header</p>;
-	}
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision</h1>
+        <h2>Put your life in the hands of a computer</h2>
+      </div>
+    );
+  }
+}
+
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        Options component here
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        AddOption component here
+      </div>
+    );
+  }
 }
 
 const jsx = (
-	<div>
-		<h1>Title</h1>
-		<Header />
-		<Header />
-		<Header />
-	</div>
+  <div>
+    <Header />
+    <Action />
+    <Options />
+    <AddOption />
+  </div>
 );
 
-ReactDOM.render( jsx, document.getElementById( 'app' ) );
+ReactDOM.render(jsx, document.getElementById('app'));
